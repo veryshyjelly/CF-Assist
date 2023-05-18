f44d94ab430e6dd0841aea6edd468ddd3c75c4ad
Ayush0-8Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2023-05-18 22:55:34 +0530

    ui mvp done

--- app/home.tsx
+++ app/home.tsx
@@ -1,40 +1,82 @@
+import { Textarea, SegmentedControl } from "@mantine/core";
+import { useState } from "react";
+
 const Home = () => {
+    const [showResult, setShowResult] = useState('testcase');
+    // const [resultor]
+
     return (
         <div>
-            <div className='mt-8 ml-20 font-semibold text-4xl tracking-widest'>
+            <div className='mt-7 ml-24 font-semibold text-4xl tracking-widest'>
                 Three Sum
+                <SegmentedControl value={showResult} onChange={setShowResult} ml={200} data={[
+                    { label: "Testcase", value: "testcase" },
+                    { label: "Result", value: "result" }
+                ]} className="bg-white/50" />
             </div>
 
-            <div className="h-[33rem] overflow-hidden">
+
+            <div className="h-[32.125rem] overflow-hidden">
 
 
-                <div className="mt-10 ml-52 h-[31rem] flex flex-row overflow-hidden">
-                    <div className="mt-16 h-[20rem] w-[18rem] font-[500] text-2xl bg-[#00b2ca] relative text-center">
-                        Answer
-                        <div className={`h-[18rem] w-[18rem] top-8 bg-[#ddccba] absolute`}></div>
-                        <div className={`h-[20rem] w-[18rem] top-[9rem] left-[18rem] bg-[#00b2ca] absolute skew-y-[45deg]`}></div>
-                        <div className={`h-[18rem] w-[18rem] top-[20rem] left-[9rem] bg-[#7dcfb6] absolute skew-x-[45deg]`}></div>
+                <div className="mt-14 h-[31rem] flex flex-row overflow-hidden">
+                    <div className="px-24 py-20">
+                        <SegmentedControl ml={200} data={[
+                            { label: "Case 1", value: "case1" },
+                            { label: "Case 2", value: "case2" }
+                        ]} className="bg-white/50 mx-auto" size="md" orientation="vertical" />
+
                     </div>
+                    {showResult === 'result' &&
+                        <>
+                            <div className="mt-12 h-[20rem] w-[18rem] font-[500] text-3xl bg-[#00b2ca] relative text-center">
+                                Answer
+                                <Textarea className={`h-[18rem] w-[18rem] px-2 top-8 bg-[#ddccba] absolute`}
+                                    variant="unstyled" maxRows={12} autosize />
+                                <div className={`h-[20rem] w-[18rem] top-[15.5rem] left-[18rem] bg-[#00b2ca] 
+                                    drop-shadow-xl absolute skew-y-[60deg]`}></div>
+                                <div className={`h-[18rem] w-[18rem] top-[20rem] left-[15.5rem] bg-[#7dcfb6] 
+                                    drop-shadow-xl absolute skew-x-[60deg]`}></div>
+                            </div>
 
-                    <div className={`ml-28 h-[20rem] w-[18rem] font-[500] text-2xl bg-[#0077ca] relative text-center`}>
-                        Output
-                        <div className={`h-[18rem] w-[18rem] top-8 bg-[#ddccba] absolute`}></div>
-                        <div className={`h-[20rem] w-[18rem] top-[9rem] left-[18rem] bg-[#0077ca] absolute skew-y-[45deg]`}></div>
-                        <div className={`h-[18rem] w-[18rem] top-[20rem] left-[9rem] bg-[#8ccf7d] absolute skew-x-[45deg]`}></div>
-                    </div>
+                            <div className={`ml-20 h-[20rem] w-[18rem] font-[500] text-3xl bg-[#0077ca] relative text-center`}>
+                                Output
+                                <Textarea className={`h-[18rem] w-[18rem] px-2 top-8 bg-[#ddccba] absolute`}
+                                    variant="unstyled" maxRows={12} autosize />
+                                <div className={`h-[20rem] w-[18rem] top-[9rem] left-[18rem] bg-[#0077ca] 
+                                    drop-shadow-xl absolute skew-y-[45deg]`}></div>
+                                <div className={`h-[18rem] w-[18rem] top-[20rem] left-[9rem] bg-[#8ccf7d] 
+                                    drop-shadow-xl absolute skew-x-[45deg]`}></div>
+                            </div>
+                        </>}
+                    {showResult === 'testcase' && <>
+                        <div className="ml-16 mt-5 h-[20rem] w-[18rem] font-[500] text-3xl bg-[#00b2ca] relative text-center">
+                            Input
+                            <Textarea className={`h-[18rem] w-[18rem] px-2 top-8 bg-[#ddccba] absolute`}
+                                variant="unstyled" maxRows={12} autosize />
+                            <div className={`h-[20rem] w-[18rem] top-[10.7rem] left-[18rem] bg-[#00b2ca] 
+                                drop-shadow-xl absolute skew-y-[50deg]`}></div>
+                            <div className={`h-[18rem] w-[18rem] top-[20rem] left-[7.6rem] bg-[#7dcfb6] 
+                                drop-shadow-xl absolute skew-x-[40deg]`}></div>
+                        </div>
+                    </>}
                 </div>
 
-                <div className="text-2xl font-medium justify-around flex flex-row -top-[5rem] relative">
-                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer hover:bg-white/60 active:bg-white/70">
+                <div className="text-2xl font-medium justify-around flex flex-row -top-[6.5rem] relative">
+                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-white/60 active:bg-white/70">
                         Test
                     </div>
-                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer hover:bg-white/60 active:bg-white/70">
+                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-white/60 active:bg-white/70">
                         Prev
                     </div>
-                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer hover:bg-white/60 active:bg-white/70">
+                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-white/60 active:bg-white/70">
                         Next
                     </div>
-                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer hover:bg-white/60 active:bg-white/70">
+                    <div className="px-12 py-2 bg-white/50 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-white/60 active:bg-white/70">
                         Done
                     </div>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import HomePage from './home'
 import { useRef, useState } from 'react'
 import { open, save } from "@tauri-apps/api/dialog"
 import { Inconsolata } from 'next/font/google'
+import { Modal, Chip, Group, RangeSlider, Input } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 
 const inconso = Inconsolata({ subsets: ['latin'] })
 
@@ -19,14 +21,71 @@ export default function Home() {
 }
 
 function Starter(props: { setStarted: Function }) {
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
-    <>
-      <div onClick={() => props.setStarted(true)}
-        className='my-52 text-center cursor-pointer rounded-full py-6 w-32 mx-auto font-semibold bg-[#159895]'>
-        Start
-      </div>
-    </>
-  )
+    <div>
+      <Modal padding={0} radius={'lg'} opened={opened} onClose={close} centered withCloseButton={false}>
+        <Tags />
+      </Modal>
+      <div className='mt-20 text-center'>
+        <div className='text-3xl font-semibold'>
+          CF Assist
+        </div>
+        <div className='my-10 flex justify-around'>
+          <div onClick={open}
+            className="text-2xl font-medium px-12 py-2 bg-white/40 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-white/50 active:bg-white/70">
+            Tags
+          </div>
 
+        </div>
+
+        <div className='w-96 mx-auto text-2xl font-semibold bg-white/30 px-8 py-5 rounded-xl'>
+          Difficulty
+          <RangeSlider
+            defaultValue={[1000, 2200]}
+            min={800}
+            max={3500}
+            step={100}
+            my={35}
+            marks={[{ value: 800, label: '800' }, { value: 1200, label: '1200' }, { value: 1600, label: '1600' },
+            { value: 2000, label: '2000' }, { value: 2400, label: '2400' }, { value: 2800, label: '2800' }, { value: 3200, label: '3200' },]}
+            labelAlwaysOn
+          />
+        </div>
+
+        <div className='w-96 mx-auto mt-10'>
+          <Input variant='filled' placeholder='Project Directory' />
+        </div>
+      </div>
+
+      <div className='mt-10 w-48 text-center mx-auto'>
+        <div onClick={() => props.setStarted(true)}
+          className="text-2xl font-medium px-12 py-2 bg-green-600/40 rounded-lg cursor-pointer select-none 
+                        hover:shadow hover:bg-green-600/50 active:bg-green-600/70">
+          Start
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+const Tags = () => {
+  const tags = ['binary search', 'bitmasks', 'brute force', 'combinatorics',
+    'data structures', 'dfs and similar', 'strings', 'trees', 'two pointers', 'ternary search', 'sortings',
+    'shortest paths', 'greedy', 'dp', 'divide and conquer', 'chinese remainder theorem', 'graphs']
+  return (
+    <div className='p-5'>
+      <Chip.Group multiple>
+        {/* <div className='flex'> */}
+        <Group>
+
+          {tags.map(v => <Chip variant='light' value={v}>{v}</Chip>)}
+        </Group>
+        {/* </div> */}
+      </Chip.Group>
+    </div>
+  )
 }
